6d93e7cbb158ef07d0ea039013cbcbbeaa2acc8d
Rone Mendes <ronemendes@rvtecnologia.com.br>
2019-07-17 17:07:49 -0300

    atualizando projetos cucumber bdd

---
+++ Cucumber - BDD/PUCVirtual/features/logar_sistema.feature
@@ -0,0 +1,15 @@
+#language: pt
+
+Funcionalidade: Autenticação Usuário
+	Logar em sistema de aprendizagem PUC Minas
+	
+Cenário: Usuário deseja acessar ambiente de aprendizagem da PUC Minas
+	Dado que eu tenho dados para autenticação cadastrados
+	Quando eu informar "usuario" e "senha" válidos
+	Então o sistema deverá me autenticar
+	E me direcionar para o ambiente de aprendizagem
+	
+Cenário: Página de login deve exibir mensagem de erro em caso de autenticação inválida
+	Dado que eu tenho dados para autenticação cadastrados
+	Quando eu informar "usuario" e "senha" inválidos
+	Então o sistema deve exibir uma "mensagem" de erro
---
+++ Cucumber - BDD/PUCVirtual/features/step_definitions/steps.rb
@@ -0,0 +1,27 @@
+Dado("que eu tenho usuário e senha cadastrado") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu informar meus dados para autenticação") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("o sistema deverá me autenticar") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("me direcionar para o ambiente de aprendizagem") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado("que eu tenho usuário e senha cadastrados") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu informar os dados inválidos") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("o sistema deve exibir uma mensagem de erro") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
