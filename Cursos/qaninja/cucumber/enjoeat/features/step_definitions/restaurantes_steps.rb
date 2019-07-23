a4c68cff7cdb8c68a723c88d6f919178cba3eba9
Rone Mendes <ronemendes@rvtecnologia.com.br>
2019-07-23 15:19:42 -0300

    up

--- Cursos/qaninja/cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
+++ Cursos/qaninja/cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
@@ -9,10 +9,10 @@ Quando("acesso a lista de restaurantes") do
 end  
   
 Então("devo ver todos os restaurantes dessa lista") do
-    restaurants = all('.restaurant-item')
+    restaurants = all('.restaurant-item')    #o '.' representa a classe. "all" vê todas as opçoes disponiveis. Busca todos os elementos. Devolve um array.
 
     @restaurant_data.each_with_index do |value, index|
-        expect(restaurants[index]). to have_text value['nome'].upcase
+        expect(restaurants[index]). to have_text value['nome'].upcase  #'upcase' coloca em maiúsculo
         expect(restaurants[index]). to have_text value['categoria']
         expect(restaurants[index]). to have_text value['entrega']
         expect(restaurants[index]). to have_text value['avaliacao']
